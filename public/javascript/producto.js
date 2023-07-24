b4290157e8a436ba8b5c33c257481bfc07cce22d
PabloMas060 <pablo.mas.dev@gmail.com>
2023-07-24 18:30:04 -0300

    fix product script functionallity

---
+++ public/javascript/producto.js
@@ -0,0 +1,10 @@
+function myFunction(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+
+    expandImg.src = imgs.src;
+  
+
+   
+    expandImg.parentElement.style.display = "block";
+  }
+
