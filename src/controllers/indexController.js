43053c39c0612f883c584fe3a8f9f24bb4793c30
PabloMas060 <pablo.mas.dev@gmail.com>
2023-08-09 15:52:27 -0300

    index cards from json

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,5 +1,11 @@
+const vinyls = require('../data/vinyls.json')
+
+
 module.exports = {
     index : (req,res) => {
-        return res.render('index')
+        return res.render('index',{
+            vinyls,
+            offerVinyls : vinyls.filter(vinyl => vinyl.discount > 0)
+        })
     }
 }
