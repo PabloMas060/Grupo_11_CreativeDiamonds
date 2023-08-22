f8c9255d8a08578659324ec4e4269497c05b1a36
Braian123548 <braiandelprado1@gmail.com>
2023-08-21 22:03:45 -0300

    Agregando delete

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,6 +1,8 @@
 const vinyls = require('../data/vinyls.json');
 const shirts = require('../data/shirts.json');
 const filtroController = require('./filtroController'); 
+const path = require('path');
+const fs = require("fs");
 
 module.exports = {
     detail: (req, res) => {
@@ -86,6 +88,53 @@ module.exports = {
         return res.render('products/indumentaria', {
             shirts: productosAMostrar
         });
+    },
+    remove: (req, res) => {
+        const idProducto = req.params.id;
+        console.log(idProducto);
+        const indexVinyl = vinyls.findIndex(product => product.id === idProducto);
+        const indexShirt = shirts.findIndex(product => product.id === idProducto);
+    
+        if (indexVinyl !== -1) {
+            const removedProduct = vinyls[indexVinyl];
+            vinyls.splice(indexVinyl, 1);
+    
+            removeProductImagesAndWriteFile(removedProduct, vinyls, 'vinyls.json');
+        } else if (indexShirt !== -1) {
+            const removedProduct = shirts[indexShirt];
+            shirts.splice(indexShirt, 1);
+    
+            removeProductImagesAndWriteFile(removedProduct, shirts, 'shirts.json');
+        } else {
+            console.log('Producto no encontrado en el array.');
+            res.status(404).send('Producto no encontrado');
+            return;
+        }
+    
+        function removeProductImagesAndWriteFile(product, productsArray, fileName) {
+            const imagesToDelete = [product.mainImage, ...(product.images || [])];
+        
+            imagesToDelete.forEach(image => {
+                const imagePath = path.resolve(__dirname, '..', '..', 'public', 'images', image);
+                fs.unlink(imagePath, err => {
+                    if (err) {
+                        console.error('Error deleting image:', err);
+                    } else {
+                        console.log('Image deleted successfully');
+                    }
+                });
+            });
+        
+            fs.writeFile(path.resolve(__dirname, '..', 'data', fileName), JSON.stringify(productsArray, null, 2), err => {
+                if (err) {
+                    console.error('Error al escribir en el archivo:', err);
+                } else {
+                    console.log('Archivo actualizado exitosamente.');
+                }
+            });
+        }
+
+        res.redirect('/users/admin');
     },
     shows: (req, res) => {
         return res.render('products/shows');
