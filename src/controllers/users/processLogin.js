243ee5d77b46604960b07dd7669b10db9d690271
EpochAndFriends <maximilianobrocato@gmail.com>
2023-09-10 19:48:14 -0300

    login funcional

--- src/controllers/users/processLogin.js
+++ src/controllers/users/processLogin.js
@@ -1,3 +1,50 @@
-module.exports = (req,res) =>{
-    return res.render('users/login')
-}
+const { validationResult } = require('express-validator');
+const { readJSON } = require('../../data');
+const bcrypt = require('bcrypt'); 
+
+module.exports = (req, res) => {
+  const errors = validationResult(req);
+  console.log("Iniciando...");
+  
+  if (errors.isEmpty()) {
+    const users = readJSON('users.json');
+    const { email, password, remember } = req.body;
+
+    const user = users.find((user) => user.email === email);
+
+    if (user && bcrypt.compareSync(password, user.password)) { 
+      req.session.user = {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        rol: user.rol,
+      };
+
+      if (remember !== undefined) {
+        // Si "Recordar Sesión" está activado, muestra un mensaje en la consola
+        console.log("Sesión recordada. Usuario:", req.session.user);
+        res.cookie('creativeDiamonds', req.session.user, {
+          maxAge: 1000 * 60 * 60 * 24 * 7,
+        });
+      }
+
+      console.log("Inicio de sesión exitoso. Usuario:", req.session.user);
+      return res.redirect('/');
+    } else {
+      console.log("Credenciales incorrectas. Correo electrónico:", email);
+      return res.render('users/login', {
+        errors: {
+          loginFailed: {
+            msg: 'Credenciales incorrectas. Por favor, verifica tu correo y contraseña.',
+          },
+        },
+      });
+    }
+  } else {
+    console.log("Errores de validación:", errors.array());
+    return res.render('users/login', {
+      errors: errors.mapped(),
+    });
+  }
+};
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const {register, processRegister,login, processLogin, profile, updateProfile, cart, admin, logout } = require('../controllers/usersController')
+const localsCheck = require('../middlewares/localsCheck');
+const { register, processRegister, login, processLogin, profile, updateProfile, cart, admin, logout } = require('../controllers/usersController');
 const registerValidation = require('../validation/registerValidation');
+const loginValidation = require('../validation/loginValidation');
 const { uploads } = require('../middlewares/uploads');
+
 /* /users */
+router.use(localsCheck);
 router.get('/register', register);
 router.post('/register', uploads.fields([
-    {
-      name: "mainImage"
-    }
-  ]),registerValidation, processRegister);
+  {
+    name: "mainImage"
+  }
+]), registerValidation, processRegister);
+
 router.get('/login', login);
-router.post('/login', processLogin);
+router.post('/login', loginValidation, processLogin);
+
 router.get('/profile', profile);
 router.put('/update-profile', updateProfile);
 router.get('/logout', logout);
 
 router.get('/admin', admin);
 router.post('/admin/:typeSearch?', admin);
-router.get('/cart', cart)
-
+router.get('/cart', cart);
 
 module.exports = router;
