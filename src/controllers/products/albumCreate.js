077904dfa78db97a266d4e608f47fad1157a0be5
Fer <cirodelpiero9@gmal.com>
2023-10-17 11:58:55 -0300

    cambios en controllers

--- src/controllers/products/albumCreate.js
+++ src/controllers/products/albumCreate.js
@@ -1,3 +1,73 @@
 const {existsSync, unlinkSync} = require ('fs')
 const {validationResult} = require('express-validator');
 const db = require ('../../database/models')
+
+module.exports = (req, res) => {
+
+    
+    const errors = validationResult(req);
+
+    if(errors.isEmpty()){
+        const {title, discography, year, price,  discount, description, exclusive, band, genre} = req.body
+        db.Product.create({
+        title : title.trim(),
+        discography : discography.trim(),
+        year,
+        price,
+        discount : discount || 0,
+        description : description.trim(),
+        exclusive,
+        bandId : band,
+        genreId : genre
+        })
+        .then(product => {
+        if(req.files.length){
+            const images = req.files.map(file => {
+            return {
+                file: file.filename,
+                main : index === 0 ? true : false,
+                productId : product.id,
+                }
+            })
+            db.Image.bulkcreate(images, {
+            validate : true
+            })
+            .then(response => console.log(response))
+        }
+        return res.redirect('/admin');
+        })
+        .catch(error => console.log(error))
+
+
+
+    }else {
+
+        if(req.files.length){
+        req.files.forEach(file => {
+        
+        
+        existsSync('./public/images/' + file.filename) && unlinkSync('./public/images/' + file.filename)
+       });
+    }
+    const genres = db.Genre.findAll({
+        order : ['name']
+    });
+
+    const bands = db.Band.findAll({
+        order : ['name']
+    });
+    Promise.all([genres, bands])
+        .then(([genres, bands]) => {
+            return res.render("albumAdd", {
+        genres,
+        bands
+            });
+        })
+        .catch (error => console.log(error))
+
+
+
+
+
+    }
+}
--- src/controllers/products/mercheCreate.js
+++ src/controllers/products/mercheCreate.js
@@ -1,3 +1,71 @@
 const {existsSync, unlinkSync} = require ('fs')
 const {validationResult} = require('express-validator');
 const db = require ('../../database/models')
+
+module.exports = (req, res) => {
+
+    
+    const errors = validationResult(req);
+
+    if(errors.isEmpty()){
+        const {name, price,  discount, description, exclusive, band, type} = req.body
+        db.Product.create({
+        name : name.trim(),
+        price,
+        discount : discount || 0,
+        description : description.trim(),
+        exclusive,
+        bandId : band,
+        typeId : type
+        })
+        .then(product => {
+        if(req.files.length){
+            const images = req.files.map(file => {
+            return {
+                file: file.filename,
+                main : index === 0 ? true : false,
+                productId : product.id,
+                }
+            })
+            db.Image.bulkcreate(images, {
+            validate : true
+            })
+            .then(response => console.log(response))
+        }
+        return res.redirect('/admin');
+        })
+        .catch(error => console.log(error))
+
+
+
+    }else {
+
+        if(req.files.length){
+        req.files.forEach(file => {
+        
+        
+        existsSync('./public/images/' + file.filename) && unlinkSync('./public/images/' + file.filename)
+       });
+    }
+    const types = db.Type.findAll({
+        order : ['name']
+    });
+
+    const bands = db.Band.findAll({
+        order : ['name']
+    });
+    Promise.all([types, bands])
+        .then(([types, bands]) => {
+            return res.render("mercheAdd", {
+        genres,
+        bands
+            });
+        })
+        .catch (error => console.log(error))
+
+
+
+
+
+    }
+}
